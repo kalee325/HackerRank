74914a6a34931059699c6fcb30961c3f3c45946f
kalee325 <33854467+kalee325@users.noreply.github.com>
2019-10-16 20:59:34 -0700

    Update oddNumbers.java

--- Sample Test/Odd Numbers!/oddNumbers.java
+++ Sample Test/Odd Numbers!/oddNumbers.java
@@ -24,6 +24,7 @@ public class Solution {
         }
         return oddArr;
     }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
