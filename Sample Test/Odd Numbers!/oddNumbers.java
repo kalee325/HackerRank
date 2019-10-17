1a92ac3ac0344a0aca8395c32f2e17ad37765904
KaHo Lee <klee6@mail.sfsu.edu>
2019-10-16 20:34:21 -0700

    Sample Test
    Upload Solution

---
+++ Sample Test/Odd Numbers!/oddNumbers.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+
+    // Complete the oddNumbers function below.
+    static List<Integer> oddNumbers(int l, int r) {
+        List<Integer> oddArr = new ArrayList<>();
+
+        for(int i = l; i <= r; i++){
+            if(i%2 != 0){
+                oddArr.add(i);
+            }
+        }
+        return oddArr;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int r = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> res = oddNumbers(l, r);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
